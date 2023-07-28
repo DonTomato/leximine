e430729f9d1cdb5b09a182c1d25e8f5849b7d7ad
Mikhail Voronov <michael.voronov@gmail.com>
2023-07-28 22:14:43 +0200

    snowball - step 2

--- fslex/leximine/Snowball.fs
+++ fslex/leximine/Snowball.fs
@@ -1,5 +1,7 @@
 module leximine.Snowball
 
+open System.Text.RegularExpressions
+
 let private lowercase (word: string) =
     word.ToLower()
     
@@ -12,13 +14,22 @@ let private removeSuffix (suffix: string) (word: string) =
 let rec private removeSuffixes word suffixes =
     suffixes |> List.fold (fun acc s -> removeSuffix s acc) word
 
+let testSuffix (word: string) (suffixPattern: string) =
+    let m = Regex($"{suffixPattern}$").Match(word)
+    match m with
+    | m when m.Success -> (true, m.Index)
+    | _ -> (false, -1)
+
 let private step_0 word =
     [ "'"; "'s"; "'s'" ] |> removeSuffixes word
     
-let private step_1b word =
-    0
+let private step_1b (word: string) =
+    match testSuffix word "sess" with
+    | (true, index) -> $"{word.Substring(0, word.Length - index)}ss"
+    | _ -> match testSuffix word ""
 
 let snowballEn text =
     text
         |> lowercase
         |> step_0
+        |> step_1b
