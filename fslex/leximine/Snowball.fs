fefbecc7e07ff5a49c6c80822f18b98757ecc04b
Mikhail Voronov <michael.voronov@gmail.com>
2023-07-28 16:02:12 +0200

    snowball - step 1 - fixes

--- fslex/leximine/Snowball.fs
+++ fslex/leximine/Snowball.fs
@@ -9,11 +9,14 @@ let private removeSuffix (suffix: string) (word: string) =
     else
         word
        
-let rec removeSuffixes word suffixes =
+let rec private removeSuffixes word suffixes =
     suffixes |> List.fold (fun acc s -> removeSuffix s acc) word
 
 let private step_0 word =
     [ "'"; "'s"; "'s'" ] |> removeSuffixes word
+    
+let private step_1b word =
+    0
 
 let snowballEn text =
     text
