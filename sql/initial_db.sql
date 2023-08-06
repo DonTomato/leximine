ac84aad18f8b9d07c1d1bcbbd3c36c7108d6a162
Mikhail Voronov <michael.voronov@gmail.com>
2023-08-06 11:13:02 +0200

    new db structure

--- sql/initial_db.sql
+++ sql/initial_db.sql
@@ -9,13 +9,10 @@ CREATE TABLE "book" (
 );
 
 CREATE TABLE "sentence" (
-	"id"	INTEGER,
-	"book_id"	INTEGER NOT NULL,
+	"id"	TEXT,
 	"sentence"	TEXT NOT NULL,
 	"words_count"	INTEGER NOT NULL DEFAULT 0,
-	"hash"	TEXT NOT NULL,
-	FOREIGN KEY("book_id") REFERENCES "book"("id"),
-	PRIMARY KEY("id" AUTOINCREMENT)
+	PRIMARY KEY("id")
 );
 
 CREATE TABLE "word" (
@@ -28,17 +25,53 @@ CREATE TABLE "word" (
 CREATE TABLE "word_form" (
 	"id"	TEXT,
 	"word_id"	TEXT NOT NULL,
+	"count"	INTEGER NOT NULL DEFAULT 0,
 	FOREIGN KEY("word_id") REFERENCES "word"("word_id"),
 	PRIMARY KEY("id")
+);
+
+CREATE TABLE "book_sentence" (
+	"id"	INTEGER,
+	"book_id"	INTEGER NOT NULL,
+	"sentence_id"	TEXT NOT NULL,
+	"count"	INTEGER NOT NULL DEFAULT 0,
+	FOREIGN KEY("book_id") REFERENCES "book"("id"),
+	FOREIGN KEY("sentence_id") REFERENCES "sentence"("id"),
+	PRIMARY KEY("id" AUTOINCREMENT)
 );
 
 CREATE TABLE "book_word" (
 	"id"	INTEGER,
 	"book_id"	INTEGER NOT NULL,
 	"word_id"	TEXT NOT NULL,
-	"sentence_id"	INTEGER NOT NULL,
 	"count"	INTEGER NOT NULL DEFAULT 0,
+	PRIMARY KEY("id" AUTOINCREMENT),
 	FOREIGN KEY("book_id") REFERENCES "book"("id"),
+	FOREIGN KEY("word_id") REFERENCES "word"("word_id")
+);
+
+CREATE TABLE "book_word_form" (
+	"id"	INTEGER,
+	"book_id"	INTEGER NOT NULL,
+	"word_form_id"	TEXT NOT NULL,
+	"count"	INTEGER NOT NULL DEFAULT 0,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("word_form_id") REFERENCES "word_form"("id"),
+	FOREIGN KEY("book_id") REFERENCES "book"("id")
+);
+
+CREATE TABLE "word_form_sentence" (
+	"id"	INTEGER,
+	"word_form_id"	TEXT NOT NULL,
+	"sentence_id"	TEXT NOT NULL,
+	FOREIGN KEY("sentence_id") REFERENCES "sentence"("id"),
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE "word_sentence" (
+	"id"	INTEGER,
+	"word_id"	TEXT NOT NULL,
+	"sentence_id"	TEXT NOT NULL,
 	FOREIGN KEY("word_id") REFERENCES "word"("word_id"),
 	FOREIGN KEY("sentence_id") REFERENCES "sentence"("id"),
 	PRIMARY KEY("id" AUTOINCREMENT)
