bfdf9abd9589de2b8d172c367636a35091b85a67
Mikhail Voronov <michael.voronov@gmail.com>
2023-08-27 20:45:50 +0200

    back - service

--- back/leximine.Logic/DbFileHelper.fs
+++ back/leximine.Logic/DbFileHelper.fs
@@ -2,8 +2,18 @@ module leximine.Logic.DbFileHelper
 
 open System.IO
 let private root = @"c:\data\leximine\"
+let private dbFileName = "leximine.db"
 
 let getLanguages = fun () ->
     Directory.GetDirectories(root)
     |> Array.map (fun x -> Path.GetFileName(x))
     |> Array.toList
+    
+let getCurrentDbFileName ln =
+    Path.Combine(root, ln, dbFileName)
+    
+let initDb ln =
+    let currentDbf = getCurrentDbFileName ln
+    if not <| File.Exists currentDbf then
+        let originalDbf = Path.Combine(root, dbFileName)
+        File.Copy(originalDbf, currentDbf)
---
+++ back/leximine.SqliteDb/Core.fs
@@ -0,0 +1,67 @@
+﻿module leximine.SqliteDb
+
+open System
+open System.Data
+open Microsoft.Data.Sqlite
+
+let createCn fileName =
+    let cn = new SqliteConnection($"Data Source={fileName}")
+    cn.Open()
+    cn
+    
+let command sql (cn: SqliteConnection) =
+    let command = cn.CreateCommand()
+    command.CommandText <- sql
+    command
+    
+let addParameter param value (command: SqliteCommand) =
+    command.Parameters.AddWithValue(param, value) |> ignore
+    command
+    
+type SqliteReaderHelper = {
+    int: string -> int
+    long: string -> int64
+    string: string -> string
+}
+    
+let query f (command: SqliteCommand) =
+    use reader = command.ExecuteReader()
+    let tableSchema = reader.GetSchemaTable()
+                  
+    let columns = tableSchema.Rows
+                  |> Seq.cast<DataRow>
+                  |> Seq.map (fun r -> (r.["ColumnOrdinal"] :?> int, r.["ColumnName"] :?> string))
+                  |> Seq.toList
+                  
+    let getIndex columnName =
+        let (i, _) = columns |> List.find (fun (i, c) -> c = columnName)
+        i
+                  
+    let r = {
+        int = fun columnName -> reader.GetInt32(getIndex columnName)
+        long = fun columnName -> reader.GetInt64(getIndex columnName)
+        string = fun columnName -> reader.GetString(getIndex columnName)
+    }
+    
+    seq {
+        while reader.Read() do
+            yield f r
+    } |> Seq.toList
+    
+let queryScalar f (command: SqliteCommand) = f (command.ExecuteScalar())
+
+let execute (command: SqliteCommand) =
+    command.ExecuteNonQuery()
+    
+// Type Helpers
+    
+let toLong (obj: Object) = obj :?> int64
+let toInt (obj: Object) = obj :?> int
+let toString (obj: Object) = obj :?> string
+
+// Helpers
+
+let getLastID cn = cn |> command "SELECT last_insert_rowid();" |> queryScalar toLong
+
+let transaction (cn: SqliteConnection) = cn.BeginTransaction()
+let commit (transaction: SqliteTransaction) = transaction.Commit() 
